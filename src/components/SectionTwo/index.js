3e3173a229b780670751c5641b4c15e92af5ea55
Ainne Oum <ainneo@gmail.com>
2021-07-15 19:37:01 -0700

    add SectionTwo

---
+++ src/components/SectionTwo/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./sectionTwo.css";
+
+export default function SectionTwo() {
+  return (
+    <div className="section-container">
+      <div className="section-content">
+        <div className="section-text">
+          <h3>SECTION TWO</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+          <span>
+            <p> > Section Two </p>
+          </span>
+        </div>
+        <div className="section-img"></div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Navbar from "../components/Navbar";
 import About from "../components/AboutSection";
 import Sidebar from "../components/Sidebar";
 import Landing from "../components/LandingSection";
+import SectionTwo from "../components/SectionTwo";
+// import SectionThree from "../components/SectionThree";
+// import SectionFour from "../components/SectionFour";
+// import SectionFive from "../components/SectionFive";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,6 +20,10 @@ export default function Home() {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <Landing />
+      <SectionTwo />
+      {/* <SectionThree />
+      <SectionFour />
+      <SectionFive /> */}
       <About />
     </>
   );
